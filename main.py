90b84ee002173500d7897acee598801e114f9810
pratebalo <REve3Man>
2021-01-29 20:43:32 +0100

    asda

--- main.py
+++ main.py
@@ -46,6 +46,17 @@ load_dotenv()
 TOKEN = os.environ.get("TOKEN")
 mode = os.environ.get("mode")
 
+# By default the install() function installs a handler on the root logger,
+# this means that log messages from your code and log messages from the
+# libraries that you use will all show up on the terminal.
+coloredlogs.install(level='INFO')
+
+# If you don't want to see log messages from libraries, you can pass a
+# specific logger object to the install() function. In this case only log
+# messages originating from that logger will show up on the terminal.
+coloredlogs.install(level='INFO', logger=logger)
+
+
 if mode == "dev":
     def run(updater):
         updater.start_polling()
