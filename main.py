49a22367568c47f0bc92a252cf61047e0ec45be3
pratebalo <REve3Man>
2021-02-04 08:33:59 +0100

    push

--- main.py
+++ main.py
@@ -108,7 +108,6 @@ def muditos(context: CallbackContext):
 
 
 def echo(update: Update, context: CallbackContext):
-    print(update.effective_chat.id)
     data = db.select("data")
     user_id = int(update.effective_user.id)
     chat_id = int(update.effective_chat.id)
