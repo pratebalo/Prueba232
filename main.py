c9dfe5e4d158b9baa2b9d797edc6d6afff81dffd
pratebalo <REve3Man>
2021-04-14 19:48:43 +0200

    .

--- main.py
+++ main.py
@@ -54,12 +54,34 @@ elif mode == "prod":
         PORT = int(os.environ.get("PORT", "8443"))
         HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
 
-        updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN,webhook_url=f"https://{HEROKU_APP_NAME}.herokuapp.com/{TOKEN}")
+        updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN, webhook_url=f"https://{HEROKU_APP_NAME}.herokuapp.com/{TOKEN}")
 else:
     sys.exit()
 
 
 def birthday(context: CallbackContext):
+    data = db.select("data")
+    fecha = datetime.today().strftime('%d/%m')
+    cumpleaneros = data[data.cumple == fecha]
+    for _, cumpleanero in cumpleaneros.iterrows():
+        tts = gTTS(cumpleanero.cumple_song, lang=cumpleanero.cumple_lang)
+        tts.save(f"Felicitacion de su majestad para {cumpleanero.apodo}.mp3")
+
+        context.bot.sendMessage(chat_id=ID_MANITOBA, parse_mode="HTML",
+                                text=f"Felicidades <b>{cumpleanero.apodo}</b>!!!!!")
+        context.bot.sendSticker(chat_id=ID_MANITOBA,
+                                sticker=cumpleanero.cumple_sticker)
+        context.bot.sendAudio(chat_id=ID_MANITOBA,
+                              audio=open(f"Felicitacion de su majestad para {cumpleanero.apodo}.mp3", "rb"))
+        if cumpleanero.genero == "M":
+            context.bot.sendMessage(chat_id=ID_MANITOBA, parse_mode="HTML",
+                                    text=f"Por seeeeerrrr tan bueeeennaa muchaaaaachaaaaa 🎉🎊🎈")
+        else:
+            context.bot.sendMessage(chat_id=ID_MANITOBA, parse_mode="HTML",
+                                    text=f"Por seeeeerrrr tan bueeeenn muchaaaaachaooooo 🎉🎊🎈")
+
+
+def birthday2(update: Update, context: CallbackContext):
     data = db.select("data")
     fecha = datetime.today().strftime('%d/%m')
     cumpleaneros = data[data.cumple == fecha]
@@ -374,7 +396,7 @@ def set_birthday3(update: Update, context: CallbackContext):
     context.user_data["cancion"] = update.message.text
     context.bot.deleteMessage(chat_id, context.user_data["oldMessage"].message_id)
     context.bot.deleteMessage(chat_id, update.message.message_id)
-    context.user_data["oldMessage"] = context.bot.sendMessage(chat_id,"idioma")
+    context.user_data["oldMessage"] = context.bot.sendMessage(chat_id, "idioma")
 
     return CUMPLE3
 
@@ -384,7 +406,7 @@ def set_birthday4(update: Update, context: CallbackContext):
     context.user_data["idioma"] = update.message.text
     context.bot.deleteMessage(chat_id, context.user_data["oldMessage"].message_id)
     context.bot.deleteMessage(chat_id, update.message.message_id)
-    context.user_data["oldMessage"] = context.bot.sendMessage(chat_id,"sticker")
+    context.user_data["oldMessage"] = context.bot.sendMessage(chat_id, "sticker")
 
     return CUMPLE4
 
@@ -463,6 +485,7 @@ if __name__ == "__main__":
     dp.add_handler(tareas.conv_handler_tareas)
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('cumples', get_birthday))
+    dp.add_handler(CommandHandler('felicitar', birthday2))
 
     dp.add_handler(MessageHandler(Filters.all, echo))
 
